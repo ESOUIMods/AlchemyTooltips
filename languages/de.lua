50c2edc5e57470dbc2fd179d01646954927a2ffd
Sharlikran <danielhmpdx16@gmail.com>
2022-12-11 18:56:57 -0800

    Version: 1.17

--- languages/de.lua
+++ languages/de.lua
@@ -1,11 +1,4 @@
-
-
-
-
-
-SafeAddString (AT_CheapestCombination, "Günstigste Kombination: ", 1)
-SafeAddString (AT_CraftingCost, "Herstellungskosten: ", 1)
-SafeAddString (AT_CantCraft, "Trank/Gift kann nicht hergestellt werden", 1)
-SafeAddString (AT_SendHelp, "Es scheint, als wurde dieser Trank/dieses Gift noch nicht zur Datenbank des Addons hinzugefügt. Falls du aushelfen möchtest, kannst diesen Trank/dieses Gift zu @Hyperioxes (PC EU) senden.", 1)
-
-
+SafeAddString(AT_CheapestCombination, "GÃ¼nstigste Kombination: ", 1)
+SafeAddString(AT_CraftingCost, "Herstellungskosten: ", 1)
+SafeAddString(AT_CantCraft, "Trank/Gift kann nicht hergestellt werden", 1)
+SafeAddString(AT_SendHelp, "Es scheint, als wurde dieser Trank/dieses Gift noch nicht zur Datenbank des Addons hinzugefÃ¼gt. Falls du aushelfen mÃ¶chtest, kannst diesen Trank/dieses Gift zu @Hyperioxes (PC EU) senden.", 1)
--- languages/pl.lua
+++ languages/pl.lua
@@ -1,11 +1,4 @@
-
-
-
-
-
-SafeAddString (AT_CheapestCombination, "Najtañsza kombinacja: ", 1)
-SafeAddString (AT_CraftingCost, "Koszt produkcji: ", 1)
-SafeAddString (AT_CantCraft, "Tego eliksiru/trucizny nie mo¿na wytworzyæ, mo¿na go tylko z³upiæ z pokonanych wrogów lub znaleŸæ w ró¿nych pojemnikach", 1)
-SafeAddString (AT_SendHelp, "Wygl¹da na to, ¿e ten eliksir/trucizna nie zosta³ jeszcze dodany do bazy danych addona. Jeœli chcesz pomóc, wyœlij jedn¹ próbkê tego eliksiru/trucizny do @Hyperioxes na PC EU. Za³¹cznik zostanie odes³anych spowrotem po spisaniu ID", 1)
-
-
+SafeAddString(AT_CheapestCombination, "NajtaÃ±sza kombinacja: ", 1)
+SafeAddString(AT_CraftingCost, "Koszt produkcji: ", 1)
+SafeAddString(AT_CantCraft, "Tego eliksiru/trucizny nie moÂ¿na wytworzyÃ¦, moÂ¿na go tylko zÂ³upiÃ¦ z pokonanych wrogÃ³w lub znaleÅ¸Ã¦ w rÃ³Â¿nych pojemnikach", 1)
+SafeAddString(AT_SendHelp, "WyglÂ¹da na to, Â¿e ten eliksir/trucizna nie zostaÂ³ jeszcze dodany do bazy danych addona. JeÅ“li chcesz pomÃ³c, wyÅ“lij jednÂ¹ prÃ³bkÃª tego eliksiru/trucizny do @Hyperioxes na PC EU. ZaÂ³Â¹cznik zostanie odesÂ³anych spowrotem po spisaniu ID", 1)
